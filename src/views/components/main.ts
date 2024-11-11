33231e65fb2499c0299dec030d44c91de77b192a
Guddu Giri <guddugiri216@example.com>
2024-11-11 11:31:32 +0530

    updated  code

---
+++ src/views/components/main.ts
@@ -0,0 +1,19 @@
+import "../../index.css";
+import navbar from "./navbar.html?raw";
+import footbar from "./footbar.html?raw";
+
+// import mobileNav from "./mobileNav.html?raw";
+
+const navElement = document.querySelector<HTMLDivElement>("#navbar");
+if (navElement) {
+  navElement.innerHTML = navbar;
+}
+
+const footbarElement = document.querySelector<HTMLDivElement>("#footbar");
+if (footbarElement) {
+  footbarElement.innerHTML = footbar;
+}
+// const mobileNavElement = document.querySelector<HTMLDivElement>("#mobileNav");
+// if (mobileNavElement) {
+//   mobileNavElement.innerHTML = mobileNav;
+// }
